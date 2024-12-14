cc9f5bb2422386db4d701a550fa2c12ab20fb7f7
dsadep <86360882+dsadep@users.noreply.github.com>
2024-12-14 18:53:21 +0300

    text editor is now class
    text editor turned to class. i did it because of function new window, that should have the same functional as main window. so now new_window func creates new object of TextEditor class and equal to main window all but title of window

--- main.py
+++ main.py
@@ -1,75 +1,80 @@
 from tkinter import *
-#from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 
-text_editor = Tk()
-text_editor.title('Text Editor')
-text_editor.geometry('1280x720')
 
-text = Text(text_editor)
-text.pack(fill=BOTH, expand=1)
+class TextEditor(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Text Editor')
+        self.geometry('1280x720')
 
-is_saved = True
+        self.text = Text(self)
+        self.text.pack(fill=BOTH, expand=1)
 
-def open_file():
-    filepath = filedialog.askopenfilename()
-    if filepath != '':
-        with open(filepath, 'r') as file:
-            text_ = file.read()
-            text.delete('1.0', END)
-            text.insert('1.0', text_)
+        self.is_saved = True
 
-def save_as():
-    global text
-    t = text.get('1.0', 'end-1c')
-    save_location = filedialog.asksaveasfilename()
-    if save_location != '':
-        file1 = open(save_location, 'w+')
-        file1.write(t)
-        file1.close()
+        self.text.bind("<<Modified>>", self.on_modify)
+
+        self.create_menus()
+
+    def create_menus(self):
+        file_menu = Menu(tearoff=0)
+        file_menu.add_command(label='New window', command=self.new_window)
+        file_menu.add_command(label='Open', command=self.open_file)
+        file_menu.add_separator()
+        file_menu.add_command(label='Save as', command=self.save_as)
+        file_menu.add_separator()
+        file_menu.add_command(label='Exit', command=self.destroy_editor)
+
+        edit_menu = Menu(tearoff=0)
+        edit_menu.add_command(label='Undo', command=self.default)  # Placeholder for future functionality
+
+        format_menu = Menu(tearoff=0)
+        format_menu.add_command(label='Wrap', command=self.default)  # Placeholder for future functionality
+        format_menu.add_command(label='Font', command=self.default)  # Placeholder for future functionality
+
+        main_menu = Menu(self)
+        main_menu.add_cascade(label='File', menu=file_menu)
+        main_menu.add_cascade(label='Edit', menu=edit_menu)
+        main_menu.add_cascade(label='Format', menu=format_menu)
+
+        self.config(menu=main_menu)
+
+    def open_file(self):
+        filepath = filedialog.askopenfilename()
+        if filepath:
+            with open(filepath, 'r') as file:
+                text_ = file.read()
+                self.text.delete('1.0', END)
+                self.text.insert('1.0', text_)
+
+    def save_as(self):
+        t = self.text.get('1.0', 'end-1c')
+        save_location = filedialog.asksaveasfilename()
+        if save_location:
+            with open(save_location, 'w+') as file1:
+                file1.write(t)
+
+    def on_modify(self, event):
+        self.is_saved = False
+        self.text.edit_modified(False)
+
+    def destroy_editor(self):
+        if not self.is_saved:
+            if messagebox.askyesno('Closing', 'File is not saved. Are you sure you want to close?'):
+                self.destroy()
+        else:
+            self.destroy()
+
+    def new_window(self):
+        new_editor = TextEditor()  # Создаем новый экземпляр TextEditor
+        new_editor.title('New Window')
+
+    def default(self):
+        messagebox.showinfo('Text Editor', 'Will be realized soon')
 
 
-def on_modify(event):
-    global is_saved
-    is_saved = False
-    text.edit_modified(False)
-
-def destroy():
-    global is_saved
-    if not is_saved:
-        if messagebox.askyesno('Closing', 'File is not saved. Are you sure want to close?'):
-            text_editor.destroy()
-    else:
-        text_editor.destroy()
-
-def default():
-    messagebox.showinfo('Text Editor', 'Will be realised soon')
-
-text.bind("<<Modified>>", on_modify)
-
-file_menu = Menu(tearoff=0)
-file_menu.add_command(label='Create', command=default)
-file_menu.add_command(label='New window', command=default)
-file_menu.add_command(label='Open', command=open_file)
-file_menu.add_separator()
-file_menu.add_command(label='Save', command=default)
-file_menu.add_command(label='Save as', command=save_as)
-file_menu.add_separator()
-file_menu.add_command(label='Exit', command=destroy)
-
-edit_menu = Menu(tearoff=0)
-edit_menu.add_command(label='', command=default)
-
-format_menu = Menu(tearoff=0)
-format_menu.add_command(label='Wrap', command=default)
-format_menu.add_command(label='Font', command=default)
-
-main_menu = Menu()
-main_menu.add_cascade(label='File', menu=file_menu)
-main_menu.add_cascade(label='Edit', menu=edit_menu)
-main_menu.add_cascade(label='Format', menu=format_menu)
-
-
-text_editor.config(menu=main_menu)
-text_editor.mainloop()
+if __name__ == '__main__':
+    app = TextEditor()
+    app.mainloop()
